197bbfd3e00f5e12b493668abcb010e6380f08e3
Vitomux <vito.morici94@gmail.com>
2026-02-25 21:29:41 -0300

    implementar notificaciones, alertas y asincronismo

---
+++ preentrega3-morici/app.js
@@ -0,0 +1,345 @@
+/* LOGICA: 
+Se trata de una app para calcular gastos entre partes. 
+Situación: en una reunión un grupo de personas sale a comprar (los voluntarios)
+y paga el total, eventualmente se calcula entre los voluntarios y los demas 
+como repartirse los gastos.
+*/
+
+const personasInput = document.getElementById("personas")
+const nombreInput = document.getElementById("nombre")
+const dineroInput = document.getElementById("dinero")
+const datosContainer = document.getElementById("datos")
+const datosTotal = document.getElementById("datosTotal")
+const totalFInal = document.getElementById("final")
+const personasFinal = document.getElementById("personasFinal")
+const parteFInal = document.getElementById("parteFinal")
+const resultados = document.getElementById("resultados")
+const succesImg = document.getElementById('succesImg');
+const historial = document.getElementById('historial')
+
+let voluntarios = [ ] 
+let nombre = ""
+let dinero = ""
+let parteVoluntario = ""
+let partePorPersonas = ""
+let personas = ""
+let gastoTotal = 0
+
+function getVoluntarios( ) {
+    nombre = nombreInput.value
+    dinero =  parseInt(dineroInput.value)
+
+        if ( !nombre || isNaN( dinero ) ){
+                nombreInput.value = "Error"
+                dineroInput.value = "Error"
+                volFail( )
+
+        } else {
+                const nuevoVoluntario = { nombre, dinero }
+                voluntarios.push(nuevoVoluntario)
+                gastoTotal += nuevoVoluntario.dinero
+                console.log(voluntarios);
+                volExito( )
+
+                datosContainer.innerHTML += `
+                        <div class = "flex flex-row p-2 justify-between mb-6 overflow-hidden h-36.5">
+                            <p><strong>Nombre:</strong> ${ nuevoVoluntario.nombre }</p>
+                            <p><strong>Aporte:</strong> $${ nuevoVoluntario.dinero }</p>
+                        </div> `;
+            
+                datosTotal.innerText= `$${ gastoTotal.toFixed(2) }`
+
+                nombreInput.value = ""
+                dineroInput.value = ""
+        }
+}
+
+function getPersonas( ){
+
+    let valorIngresado = parseInt(personasInput.value)
+    
+        if ( isNaN(valorIngresado) ) {
+                personasInput.value = "Error"
+                Toastify({
+                text: "Revise valores ingresados",
+                duration: 1000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "bottom", 
+                position: "right", 
+                stopOnFocus: true,
+                style: {
+                background: "#D83D00"
+                },
+                onClick: ( ) => { }
+                }).showToast( );
+
+        } else { 
+                valorIngresado
+                personas = valorIngresado
+                console.log(personas);
+                Toastify({
+                text: "Personas sumadas correctamente",
+                duration: 1000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "bottom", 
+                position: "right", 
+                stopOnFocus: true,
+                style: {
+                background: "#00a18e"
+                },
+                onClick: ( ) => { }
+                }).showToast( );
+
+                succesImg.removeAttribute('hidden')
+                personasInput.setAttribute('hidden', '')
+                return personas
+    }
+}
+
+async function getData( ) {
+
+    const result = await Swal.fire({
+        title: "¿Desea calcular los gastos?",
+        showDenyButton: true,
+        confirmButtonText: "Si, calcular",
+        confirmButtonColor: "#00A18E",
+        denyButtonText: `No`,
+        denyButtonColor: "#D83D00"
+    });
+
+    if ( result.isConfirmed ){
+
+        Swal.fire({
+            title: 'Calculando aportes...',
+            allowOutsideClick: false,
+            didOpen: ( ) => {
+                Swal.showLoading( );
+            }
+
+        });
+        await new Promise( resolve => setTimeout( resolve, 1500 ));
+
+        totalFInal.innerText = `$${gastoTotal}`;
+        personasFinal.innerText = personas;
+        partePorPersonas = ( gastoTotal / personas ).toFixed( 2 );
+        parteFInal.innerText = `$${partePorPersonas}`;
+
+        resultados.innerHTML = "";
+
+        voluntarios.forEach(( voluntario )=>{
+            let parteVoluntario = ( voluntario.dinero - partePorPersonas ).toFixed( 2 );
+            
+            if ( parteVoluntario <= 0 ) {
+
+                resultados.innerHTML += `
+                    <div class="flex flex-row p-2 justify-between mb-6 overflow-hidden h-36.5">
+                        <p><strong>${voluntario.nombre}</strong> debe</p>
+                        <p> $${parteVoluntario * -1}</p>
+                    </div>`;
+
+            } else {
+
+                resultados.innerHTML += `
+                    <div class="flex flex-row p-2 justify-between mb-6 overflow-hidden h-36.5">
+                        <p>A <strong>${voluntario.nombre}</strong> le deben </p>
+                        <p>$${parteVoluntario}</p>
+                    </div>`;
+            }
+
+        });
+
+        await Swal.fire({
+            title: "¡Cuentas Claras!",
+            icon: "success",
+            confirmButtonColor: "#00A18E",
+        });
+    }
+}
+
+function restart( ){
+    voluntarios = [ ] 
+    nombre = ""
+    dinero = ""
+    parteVoluntario = ""
+    partePorPersonas = ""
+    personas = ""
+    gastoTotal = 0
+    
+    totalFInal.innerText = "$00.00"   
+    personasFinal.innerText = "00"
+    parteFInal.innerText = "$00.00"
+    resultados.innerHTML = ""
+    datosContainer.innerHTML = ""
+    datosTotal.innerText= ""
+    personasInput.value = ""
+    personasInput.removeAttribute('hidden')
+    succesImg.setAttribute('hidden', '')
+
+}
+
+function save( ){
+    Swal.fire({
+        title: "¿Desea guardar la operación?",
+        showDenyButton: true,
+        confirmButtonText: "Si, guardar",
+        confirmButtonColor: "#00A18E",
+        denyButtonText: `No es necesario`,
+        denyButtonColor: "#D83D00"
+
+        }).then(( result ) => {
+
+        if (result.isConfirmed) {
+                let fecha = new Date
+                localStorage.setItem('Dia', fecha.toLocaleDateString( ))
+                localStorage.setItem('Hora', fecha.toLocaleTimeString( ))
+                localStorage.setItem('Total', gastoTotal)
+                localStorage.setItem('Personas', personas)
+                localStorage.setItem('Por persona', partePorPersonas)
+                localStorage.setItem('Voluntarios', JSON.stringify( voluntarios ) )
+
+                let savedDay = localStorage.getItem('Dia')
+                let savedHour = localStorage.getItem('Hora')
+                let savedTotal = localStorage.getItem('Total')
+                let savedPersonas = localStorage.getItem('Personas')
+                let savedParte = localStorage.getItem('Por persona')
+                let savedVoluntarios = JSON.parse(localStorage.getItem('Voluntarios'))
+
+                historial.innerHTML += `
+                <div class="p-4 justify-between">
+                    <p><strong>${ savedDay }</strong> a las <strong>${savedHour}</strong></p>
+                    <p><strong>Gasto total</strong> $${ savedTotal }</p>
+                    <p><strong>Personas</strong> ${ savedPersonas }</p>
+                    <p><strong>cada uno gastó </strong>$${ savedParte }</p>
+                    
+                    <strong>Voluntarios:</strong>
+                    <div class="p-4">
+                        <ul>
+                            ${ savedVoluntarios.map( v => `
+                                <li>
+                                    <strong>${v.nombre}:</strong> $${ v.dinero - savedParte}
+                                </li>`).join( ' ' ) }
+                        </ul>
+                    </div>
+                </div>
+                <div class="h-[0.5px] bg-[#00A18E]"></div>
+            `
+                restart( ) 
+                Swal.fire({
+                    title: "Guardaste la operacion en el historial",
+                    icon: "success",
+                    confirmButtonColor: "#00A18E",
+            });
+        } else if (result.isDenied) {
+    }
+})
+}
+
+function deleteHistory( ){
+    Swal.fire({
+        title: "¿Seguro desea borrar el historial?",
+        showDenyButton: true,
+        confirmButtonText: "Si, borrar historial",
+        confirmButtonColor: "#00A18E",
+        denyButtonText: `No`,
+        denyButtonColor: "#D83D00"
+
+        }).then((result) => {
+
+        if (result.isConfirmed) {
+                historial.innerHTML = " "
+                localStorage.clear( )
+                Swal.fire({
+                    title: "Borraste el historial con éxito",
+                    icon: "success",
+                    confirmButtonColor: "#00A18E",
+            });
+        } else if (result.isDenied) {
+    }
+})
+}
+
+function volExito( ){
+    Toastify({
+    text: "Voluntario sumado correctamente",
+    duration: 1000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom", 
+    position: "right", 
+    stopOnFocus: true,
+    style: {
+    background: "#00a19e"
+    },
+    onClick: ( ) => { }
+    }).showToast( );
+}
+
+function volFail( ){
+    Toastify({
+    text: "Reingrese datos correctamente",
+    duration: 1000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom", 
+    position: "right", 
+    stopOnFocus: true,
+    style: {
+    background: "#D83D00"
+    },
+    onClick: ( ) => { }
+    }).showToast( );
+}
+
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach( button => {
+    
+    button.addEventListener( 'click', async function () {
+
+            if (button.innerText === "Sumar Voluntarios") {
+                getVoluntarios()
+
+            } else if (button.innerText === "Confirmar") {
+                getPersonas()
+
+            } else if (button.innerText === "Calcular") {
+                getData()
+
+            } else if (button.innerText === "Reiniciar") {
+                Swal.fire({
+                    title: "¿Seguro desea reiniciar la operación?",
+                    showDenyButton: true,
+                    confirmButtonText: "Si, reiniciar operación",
+                    confirmButtonColor: "#00A18E",
+                    denyButtonText: `No`,
+                    denyButtonColor: "#D83D00"
+                }).then((result) => {
+
+                    if (result.isConfirmed) {
+                        restart()
+                        Swal.fire({
+                            title: "Todos los valores volvieron a cero",
+                            icon: "success",
+                            confirmButtonColor: "#00A18E",
+                        })
+                    } else if (result.isDenied) {
+                    }
+                })
+            } else if (button.innerText === "Guardar") {
+                save()
+                console.log(localStorage)
+
+            } else if (button.innerText === "Borrar historial") {
+                deleteHistory()
+                console.clear()
+            }
+        })
+    
+})
